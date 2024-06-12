9789f5d96f40543ceda0300329cf1578f4e10408
jmsb02 <jmsb02@naver.com>
2024-06-12 15:46:36 +0900

    commit 0612

--- src/main/java/AlgorithmCodingTest/Search/Search028.java
+++ src/main/java/AlgorithmCodingTest/Search/Search028.java
@@ -3,57 +3,89 @@ package AlgorithmCodingTest.Search;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Search028 {
 
-    static int n, m;
-    static int[][] A;
-    static boolean[][] visited;
-
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
+    static int n;
+    static ArrayList<Edge>[] A;
+    static boolean[] visited;
+    static int[] distance;
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken()); //x
-        m = Integer.parseInt(st.nextToken()); //y
-        A = new int[n][m];
+        n = Integer.parseInt(br.readLine());
 
-        visited = new boolean[n][m];
+        A = new ArrayList[n + 1];
 
-        //2차원 배열 초기화
-        for (int i = 0; i < n; i++) {
-            String text = br.readLine();
-            for (int j = 0; j < m; j++) {
-                A[i][j] = text.charAt(j) - '0';
+        //인접 리스트 Edge 형식으로 초기화
+        for (int i = 1; i <= n; i++) {
+            A[i] = new ArrayList<Edge>();
+        }
+
+        //인접 리스트 값 채워 넣기
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int input = Integer.parseInt(st.nextToken());
+            while (true) {
+                int node1 = input;
+                int node2 = Integer.parseInt(st.nextToken());
+                if(node2 == -1) {
+                    break;
+                }
+                int edge = Integer.parseInt(st.nextToken());
+                A[node1].add(new Edge(node2, edge));
             }
         }
-        BFS(0,0); //(0,0) ~ (n-1,m-1)
-        System.out.println(A[n-1][m-1]);
+        distance = new int[n+1];
+        visited =new boolean[n +1];
+        BFS(1);
+
+        //bfs 한 번 다 돌렸으니 가장 먼 노드 인덱스 찾기
+        int max = 1;
+        for(int i=2; i<=n;i++) {
+            if(distance[max] < distance[i]) {
+                max = i;
+            } //max = 가장 먼 노드의 인덱스 -> 두 번째 bfs의 시작점
+        }
+
+        distance = new int[n+1];
+        visited = new boolean[n+1];
+        BFS(max);
+        Arrays.sort(distance);
+        System.out.println(distance[n]);
+
     }
 
-    private static void BFS(int i, int j) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {i,j});
-        visited[i][j] = true;
+    private static void BFS(int index) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(index);
+        visited[index] = true;
         while(!queue.isEmpty()) {
-            int[] now = queue.poll();
-            for(int k=0;k<4;k++) {
-                int x = now[0] + dx[k];
-                int y = now[1] + dy[k];
-                if (x>=0 && y>=0 && x<n && y<m) {
-                    if(A[x][y] != 0 && !visited[x][y]) {
-                        visited[x][y] = true;
-                        A[x][y] = A[now[0]][now[1]] + 1;
-                        queue.add(new int[] {x,y});
-                    }
+            int now_node = queue.poll();
+            for(Edge i : A[now_node]) {
+                int e = i.e;
+                int v = i.value;
+                if (!visited[e]) {
+                    queue.add(e);
+                    visited[e] = true;
+                    distance[e] = distance[now_node] + v; //거리 배열 업데이트
                 }
             }
+        }
+    }
+
+
+    //엣지 -> 값, 가중치
+    private static class Edge {
+        int e;
+        int value;
+
+        public Edge(int e, int value) {
+            this.e = e;
+            this.value = value;
         }
     }
 }
