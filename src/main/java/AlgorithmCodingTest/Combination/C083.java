df6cec2faa1b22dc56d76e4bc9bef670bb0b0e9e
jaexxin <jmsb02@naver.com>
2024-08-30 12:02:21 +0900

    commit 0830

--- src/main/java/AlgorithmCodingTest/Combination/C083.java
+++ src/main/java/AlgorithmCodingTest/Combination/C083.java
@@ -12,10 +12,13 @@ public class C083 {
         long mod = 1000000000;
         long[] D = new long[1000001];
 
+        //한 명일 때는 선물 교환 안 됨
         D[1] = 0;
+        //두 명일 때는 선물 교환 양방향으로만
         D[2] = 1;
 
         for (int i = 3; i <= n; i++) {
+            //한 명을 기준으로 삼고 (양방향 + 단방향) 경우의 수 곱해주면 됨
             D[i] = (i - 1) * (D[i - 1] + D[i - 2]) % mod;
         }
         System.out.println(D[n]);
